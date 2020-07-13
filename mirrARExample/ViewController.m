80d0a14ab32d1cd4f86e92684cd574e732e2ff70
Krishna Datt Shukla <kshukla@styledotme.com>
2020-07-13 17:06:26 +0530

    Added framework after removing AppDelegate from it.

--- mirrARExample/ViewController.m
+++ mirrARExample/ViewController.m
@@ -21,67 +21,39 @@
     NSDictionary *options =  @{
         @"productData": @{
                 @"Necklaces": @{
-                        @"items": @[@"502516NFEAAB32_N", @"5027182ADABA02_N", @"500005PFAAAA09"],
+                        @"items": @[@"1", @"2", @"3"],
                         @"type": @"neck"
                 },
                 @"Earrings": @{
-                        @"items": @[@"500005DMAABA04", @"500005SFAABA02"],
+                        @"items": @[@"1", @"2"],
                         @"type": @"ear"
                 },
                 @"Mangalsutras": @{
-                        @"items": @[@"502118YEEAAA32", @"502118YENAAA32", @"502118YEOAAA32"],
+                        @"items": @[@"1", @"2", @"3"],
                         @"type": @"neck"
                 },
                 @"NecklaceSets": @{
-                        @"items": @[@"5027182ADABA02", @"5027182AJABA02"],
+                        @"items": @[@"1", @"2"],
                         @"type": @"set"
                 },
                 @"PendantSets": @{
-                        @"items": @[@"5031181DXAAA09", @"5132181BUABA00"],
+                        @"items": @[@"1", @"2"],
                         @"type": @"set"
                 }
         }
     };
-    NSDictionary *loginParam = @{@"username": @"tanishqwebar",
-        @"password": @"w4y0EihepyIAA50U",
-        @"type": @"ios_sdk"
-      };
-    
-    
-    //For sakshi test
-    /*
-    NSDictionary *options = @{
-        @"productData": @{
-                @"Bracelets": @{
-                        @"items": @[@"BR-01", @"BR-02", @"BR-03"],
-                        @"type": @"wrist"
-                },
-                @"Earrings": @{
-                        @"items": @[@"1503677279384_RIB_2113_1", @"CT-2032", @"CS2124E"],
-                        @"type": @"ear"
-                },
-                @"Rings": @{
-                        @"items": @[@"RN-01", @"RN-013", @"RN-01543"],
-                        @"type": @"finger"
-                },
-                @"Sets": @{
-                        @"items": @[@"CS-414"],
-                        @"type": @"set"
-                }
-        }
+    NSDictionary *loginParam = @{@"username": @"brand username here",
+                                 @"password": @"brand password here",
+                                 @"type": @"ios_sdk"
     };
+        
     
     
-    NSDictionary *loginParam = @{@"username": @"sakshi",
-        @"password": @"w4y0EihepyIAA50U",
-        @"type": @"ios_sdk"
-      };
-    */
-    self.camera = [[MARCameraViewController alloc] init];
+    //Initialize SDK
+    self.camera = [MARCameraViewController sharedInstance];
     self.camera.productData = options;
     self.camera.loginParams = loginParam;
     [self.navigationController pushViewController:self.camera animated:NO];
-
     
 }
 
