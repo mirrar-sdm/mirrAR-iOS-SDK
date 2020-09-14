3c9e78a73604101ddcbacd1a8a9b7dda81cdad34
Krishna Datt Shukla <kshukla@styledotme.com>
2020-09-14 23:14:16 +0530

    R&D on framework signing

--- mirrARExample/ViewController.m
+++ mirrARExample/ViewController.m
@@ -18,35 +18,63 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+//    NSDictionary *options =  @{
+//        @"productData": @{
+//                @"Necklaces": @{
+//                        @"items": @[@"1", @"2", @"3"],
+//                        @"type": @"neck"
+//                },
+//                @"Earrings": @{
+//                        @"items": @[@"1", @"2"],
+//                        @"type": @"ear"
+//                },
+//                @"Mangalsutras": @{
+//                        @"items": @[@"1", @"2", @"3"],
+//                        @"type": @"neck"
+//                },
+//                @"NecklaceSets": @{
+//                        @"items": @[@"1", @"2"],
+//                        @"type": @"set"
+//                },
+//                @"PendantSets": @{
+//                        @"items": @[@"1", @"2"],
+//                        @"type": @"set"
+//                }
+//        }
+//    };
+//    NSDictionary *loginParam = @{@"username": @"brand username here",
+//                                 @"password": @"brand password here",
+//                                 @"type": @"ios_sdk"
+//    };
+    
     NSDictionary *options =  @{
         @"productData": @{
                 @"Necklaces": @{
-                        @"items": @[@"1", @"2", @"3"],
+                        @"items": @[@"502516NFEAAB32_N", @"5027182ADABA02_N", @"500005PFAAAA09"],
                         @"type": @"neck"
                 },
                 @"Earrings": @{
-                        @"items": @[@"1", @"2"],
+                        @"items": @[@"500005DMAABA04", @"500005SFAABA02"],
                         @"type": @"ear"
                 },
                 @"Mangalsutras": @{
-                        @"items": @[@"1", @"2", @"3"],
+                        @"items": @[@"502118YEEAAA32", @"502118YENAAA32", @"502118YEOAAA32"],
                         @"type": @"neck"
                 },
                 @"NecklaceSets": @{
-                        @"items": @[@"1", @"2"],
+                        @"items": @[@"5027182ADABA02", @"5027182AJABA02"],
                         @"type": @"set"
                 },
                 @"PendantSets": @{
-                        @"items": @[@"1", @"2"],
+                        @"items": @[@"5031181DXAAA09", @"5132181BUABA00"],
                         @"type": @"set"
                 }
         }
     };
-    NSDictionary *loginParam = @{@"username": @"brand username here",
-                                 @"password": @"brand password here",
-                                 @"type": @"ios_sdk"
-    };
-    
+    NSDictionary *loginParam = @{@"username": @"tanishqwebar",
+        @"password": @"w4y0EihepyIAA50U",
+        @"type": @"ios_sdk"
+      };
 
     
     
