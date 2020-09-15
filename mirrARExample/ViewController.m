4410b443e98d014726bb1763476ac860004ead0a
Krishna Datt Shukla <kshukla@styledotme.com>
2020-09-15 11:10:34 +0530

    Fixed Issues
    Removed Handtracker Framework for now.
    Fixed Memory Issues.
    Set Minimum OS version to iOS 11.

--- mirrARExample/ViewController.m
+++ mirrARExample/ViewController.m
@@ -18,65 +18,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-//    NSDictionary *options =  @{
-//        @"productData": @{
-//                @"Necklaces": @{
-//                        @"items": @[@"1", @"2", @"3"],
-//                        @"type": @"neck"
-//                },
-//                @"Earrings": @{
-//                        @"items": @[@"1", @"2"],
-//                        @"type": @"ear"
-//                },
-//                @"Mangalsutras": @{
-//                        @"items": @[@"1", @"2", @"3"],
-//                        @"type": @"neck"
-//                },
-//                @"NecklaceSets": @{
-//                        @"items": @[@"1", @"2"],
-//                        @"type": @"set"
-//                },
-//                @"PendantSets": @{
-//                        @"items": @[@"1", @"2"],
-//                        @"type": @"set"
-//                }
-//        }
-//    };
-//    NSDictionary *loginParam = @{@"username": @"brand username here",
-//                                 @"password": @"brand password here",
-//                                 @"type": @"ios_sdk"
-//    };
-    
     NSDictionary *options =  @{
         @"productData": @{
                 @"Necklaces": @{
-                        @"items": @[@"502516NFEAAB32_N", @"5027182ADABA02_N", @"500005PFAAAA09"],
+                        @"items": @[@"1", @"2", @"3"],
                         @"type": @"neck"
                 },
                 @"Earrings": @{
-                        @"items": @[@"500005DMAABA04", @"500005SFAABA02"],
+                        @"items": @[@"1", @"2"],
                         @"type": @"ear"
                 },
                 @"Mangalsutras": @{
-                        @"items": @[@"502118YEEAAA32", @"502118YENAAA32", @"502118YEOAAA32"],
+                        @"items": @[@"1", @"2", @"3"],
                         @"type": @"neck"
                 },
                 @"NecklaceSets": @{
-                        @"items": @[@"5027182ADABA02", @"5027182AJABA02"],
+                        @"items": @[@"1", @"2"],
                         @"type": @"set"
                 },
                 @"PendantSets": @{
-                        @"items": @[@"5031181DXAAA09", @"5132181BUABA00"],
+                        @"items": @[@"1", @"2"],
                         @"type": @"set"
                 }
         }
     };
-    NSDictionary *loginParam = @{@"username": @"tanishqwebar",
-        @"password": @"w4y0EihepyIAA50U",
-        @"type": @"ios_sdk"
-      };
-
     
+    NSDictionary *loginParam = @{@"username": @"brand username here",
+                                 @"password": @"brand password here",
+                                 @"type": @"ios_sdk"
+    };
     
     //Initialize SDK
     self.camera = [MARCameraViewController sharedInstance];
